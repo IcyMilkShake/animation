44ad208588c1593fbc64e20e261935d881e4c09b
MilkShake <sirapat.sermcheep@gmail.com>
2025-04-02 16:29:31 +0700

    a

--- server.js
+++ server.js
@@ -44,5 +44,5 @@ const server = http.createServer((req, res) => {
 
 const PORT = 8080;
 server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log("Server is running on PORT:", PORT);
 });
