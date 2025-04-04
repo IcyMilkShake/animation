1f8416248675f8f60bcb9480724e1b4aa92a4ccb
MilkShake <sirapat.sermcheep@gmail.com>
2025-04-04 12:36:19 +0700

    a

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
 
 // Function to hide loading screen from within Three.js
 function hideLoading() {
